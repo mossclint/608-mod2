2b9d3287d661c4713ade028683a6df7fce932ad6
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-18 20:04:15 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,5 +1,2 @@
 values = [47, 95, 88, 73, 88, 84]
 sum(values)
-print('Mean is',mean(47, 95, 88, 73, 88, 84))
-print('Median is',median(47, 95, 88, 73, 88, 84))
-print('Mode is',mode(47, 95, 88, 73, 88, 84))
