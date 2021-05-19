802b71e3bafae68439b2aa8b507b12ff40731daa
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-18 19:26:17 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,1 +1,5 @@
-count(47, 95, 88, 73, 88, 84)
+print('Count is',count(47, 95, 88, 73, 88, 84))
+print('Sum is',sum(47, 95, 88, 73, 88, 84))
+print('Mean is',mean(47, 95, 88, 73, 88, 84))
+print('Median is',median(47, 95, 88, 73, 88, 84))
+print('Mode is',mode(47, 95, 88, 73, 88, 84))
