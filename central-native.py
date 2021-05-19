00fd28a428a04b9cd8c736551ca96175637d5509
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-18 19:58:48 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,6 +1,5 @@
 values = [47, 95, 88, 73, 88, 84]
-srting.count(values)
-print('Sum is',sum(47, 95, 88, 73, 88, 84))
+print('Sum is',sum(values))
 print('Mean is',mean(47, 95, 88, 73, 88, 84))
 print('Median is',median(47, 95, 88, 73, 88, 84))
 print('Mode is',mode(47, 95, 88, 73, 88, 84))
