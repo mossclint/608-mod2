2c988f429f550c17dce784837da95a560eb334b1
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-21 20:14:45 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -3,3 +3,14 @@ print(len(values))
 print(sum(values))
 print(sum(values)/len(values))
 
+xmedian = len(values)
+values.sort()
+  
+if xmedian % 2 == 0:
+    median1 = values[xmedian//2]
+    median2 = values[xmedian//2 - 1]
+    median = (median1 + median2)/2
+else:
+    median = values[xmedian//2]
+print("Median is: " + str(median))
+
