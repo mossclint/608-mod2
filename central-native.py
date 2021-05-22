a6752a8c4fb91f167e01e6432de88e28d1f2a471
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-22 04:56:44 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -16,7 +16,7 @@ print("Median is: " + str(median))
 
 xmode = len(values)
   
-data = Counter(values)
+data = len(values)
 get_mode = dict(data)
 mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
   
