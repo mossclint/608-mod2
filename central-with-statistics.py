55effa293c9639ffa0deee4e567671390c7503a7
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-23 05:46:26 -0700

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -3,6 +3,6 @@ import statistics
 
 values = [47, 95, 88, 73, 88, 84]
 
-print("Mean using statistics is" statistics.mean(values))
+print("Mean using statistics is" + statistics.mean(values))
 print(statistics.median(values))
 print(statistics.mode(values))
