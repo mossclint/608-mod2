8c1acbbcbe050d790a75f1e175c3df9c3ccd3b78
mossclint <84056645+mossclint@users.noreply.github.com>
2021-05-23 19:18:53 -0700

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,6 +1,6 @@
 values = [47, 95, 88, 73, 88, 84]
-print(len(values))
-print(sum(values))
+print('Count is', + len(values))
+print('Sum is', + sum(values))
 print(sum(values)/len(values))
 
 xmedian = len(values)
@@ -15,19 +15,4 @@ else:
 print("Median is: " + str(median))
 
 
-from collections import Counter
-  
-# list of elements to calculate mode
-n_num = [values]
-n = len(n_num)
-  
-data = Counter(n_num)
-get_mode = dict(data)
-mode = [k for k, v in get_mode.items() if v == max(list(data.values()))]
-  
-if len(mode) == n:
-    get_mode = "No mode found"
-else:
-    get_mode = "Mode is / are: " + ', '.join(map(str, mode))
-      
-print(get_mode)
+
